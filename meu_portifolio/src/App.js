57f00117f2ab66507e8d4289e23487bd18fd4f07
diegod21 <diegousouib@gmail.com>
2024-01-07 15:04:28 -0300

    starting

---
+++ meu_portifolio/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     <div className='mainInfo'>
+      <div className="mainContent">
+        <div className="title">Olá Eu Sou um Desenvolvedor Web</div>
+        <div className="paragrafo">Eu Sou Diego Carvalho. Eu Crio Sistemas Web Frontend<br></br>Usando Modernas Tecnologias.</div>
+        <div className="button">Vamos Trabalhar Juntos!</div>
+        <div className="footer">Me Encontre No <a href="">GitHub</a> and <a href="">Linkedin</a><br></br>Baixe Meu <a href="">Currículo</a></div>
+      </div>
+     </div>
+     <div className='scroll'>
+      <div className='content uno'>
+
+      </div>
+      <div className='content dos'>
+
+      </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
